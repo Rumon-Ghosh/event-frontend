beea505bd3a1c553c56c607aa40ef134df924878
Rumon-Ghosh <rumon698@gmail.com>
2026-03-20 19:56:48 +0600

    added the register page successfully

--- src/app/(auth)/register/page.tsx
+++ src/app/(auth)/register/page.tsx
@@ -4,7 +4,7 @@ import React from 'react';
 const RegisterPage = () => {
   return (
     <div className='flex flex-col items-center justify-center bg-base-200 text-base-content max-w-4xl mx-auto p-10 my-3 rounded-2xl'>
-      <h2>Register page</h2>
+      <h2 className='text-2xl font-semibold my-3'>Wellcome to EventEra !</h2>
       <RegisterForm></RegisterForm>
     </div>
   );
--- src/components/form/RegisterForm.tsx
+++ src/components/form/RegisterForm.tsx
@@ -1,32 +1,52 @@
 "use client";
 
 import { useRouter } from "next/navigation";
+import { useForm } from "react-hook-form";
+
+type Inputs = {
+  name: string;
+  email: string;
+  password: string;
+  image: string;
+}
 
 const RegisterForm = () => {
   const router = useRouter();
+
+  const { register, handleSubmit, formState: { errors } } = useForm<Inputs>();
+
+  const handleRegister = (data: Inputs): void => {
+    console.log(data)
+  }
+
   return (
     <div className="relative">
       <button
-        className="absolute -top-12 -left-12 cursor-pointer"
+        className="absolute -top-20 -left-20 cursor-pointer"
         onClick={() => router.back()}>
        ⬅️ Back to 
       </button>
-      <form className="space-y-2">
+      <form
+        onSubmit={handleSubmit(handleRegister)}
+        className="space-y-2">
         {/* <div>
           <label className="label font-semibold" htmlFor="file">Upload Image</label>
           <input className="file mt-0.5" placeholder="Profile Image" type="file" id="file" />
         </div> */}
         <div>
           <label className="label font-semibold" htmlFor="name">Name</label>
-          <input className="input mt-0.5" placeholder="Your Name" type="text" id="name" />
+          <input className="input mt-0.5" placeholder="Your Name" type="text" id="name" {...register("name",  { required: true })} />
+          {errors.name && <p className="text-sm mt-px">Name field is required</p>}
         </div>
         <div>
           <label className="label font-semibold" htmlFor="email">Email</label>
-          <input className="input mt-0.5" placeholder="Your Email" type="email" id="email" />
+          <input className="input mt-0.5" placeholder="Your Email" type="email" id="email" {...register("email", { required: true })} />
+          {errors.email && <p className="text-sm mt-px">Email field is required</p>}
         </div>
         <div>
           <label className="label font-semibold" htmlFor="password">Password</label>
-          <input className="input mt-0.5" placeholder="Your Password" type="password" id="password" />
+          <input className="input mt-0.5" placeholder="Your Password" type="password" id="password" {...register("password", {required: true})} />
+          {errors.password && <p className="text-sm mt-px">Password field is required</p>}
         </div>
         <div className="mt-4">
           <input className="btn btn-primary w-full" type="submit" value="Register" />

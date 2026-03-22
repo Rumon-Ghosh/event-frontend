8cd13725c463a85331ebb5d41b12dd6cce3167a1
Rumon-Ghosh <rumon698@gmail.com>
2026-03-22 13:01:00 +0600

    added the navbar of home layout

--- src/app/(home)/layout.tsx
+++ src/app/(home)/layout.tsx
@@ -1,7 +1,7 @@
 export default function HomeLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return <div className="flex min-h-screen flex-col">{children}</div>;
 }
--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,8 +1,9 @@
+import Navbar from "@/components/shared/Navbar";
 
 export default function Home() {
   return (
-    <div>
-      Home Page
-    </div>
+    <>
+      <Navbar></Navbar>
+    </>
   );
 }

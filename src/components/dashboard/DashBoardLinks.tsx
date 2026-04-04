0aaf61f6f267bc2547050702d5ee781a47e537fd
Rumon-Ghosh <rumon698@gmail.com>
2026-04-04 19:09:16 +0600

    again changed dashboard links

--- src/components/dashboard/DashBoardLinks.tsx
+++ src/components/dashboard/DashBoardLinks.tsx
@@ -7,6 +7,9 @@ const DashBoardLinks = () => {
   const { user } = useAuth();
   return (
     <>
+      <li>
+        <NavLink href="/dashboard">Overview</NavLink>
+      </li>
       {user?.role === "user" && (
         <>
           <li>
@@ -32,9 +35,6 @@ const DashBoardLinks = () => {
           <li>
             <NavLink href="/dashboard/manage-orders">Manage Orders</NavLink>
           </li>
-          <li>
-        <NavLink href="/dashboard">Overview</NavLink>
-      </li>
         </>
       )}
       <li>
